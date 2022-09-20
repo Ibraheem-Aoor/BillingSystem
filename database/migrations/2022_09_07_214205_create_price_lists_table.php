179b48161b3ed03fe76123ea74493e94ed2ad2f4
Ibraheem <ibraheem.alaoor@hotmail.com>
2022-09-20 04:37:12 +0300

    up

--- database/migrations/2022_09_07_214205_create_price_lists_table.php
+++ database/migrations/2022_09_07_214205_create_price_lists_table.php
@@ -15,7 +15,7 @@ class CreatePriceListsTable extends Migration
     {
         Schema::create('price_lists', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('customer_id')->references('id')->on('customers')->onDelete('cascade');
+            $table->foreignId('customer_id')->references('id')->on('customers')->constrained()->onDelete('cascade');
             $table->foreignId('product_service_id')->references('id')->on('product_services')->onDelete('cascade');
             $table->double('selling_price');
             $table->double('vat')->default(0);
